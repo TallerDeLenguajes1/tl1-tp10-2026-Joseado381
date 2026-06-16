5ae546bb74758a4cf33ed88a8532a6dba995553f
Joseado381 <josealonzo.jaa@gmail.com>
2026-06-16 13:14:49 -0300

    commit

--- Tareas/Program.cs
+++ Tareas/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Text.Json;
+
+string urlBase = "https://jsonplaceholder.typicode.com/todos";
+
+var client = new HttpClient();
+
+var response = await client.GetAsync(urlBase);
+var strResponse = await response.Content.ReadAsStringAsync();
+
+List<Tareas> objResponse = JsonSerializer.Deserialize<List<Tareas>>(strResponse);
+
+string contenido = "";
+
+foreach (var item in objResponse)
+{
+    System.Console.WriteLine($"UserID: {item.UserID} - ID: {item.Id} - Título: {item.Title} - Completada: {item.Completed}\n");    
+}
+
+string path = @"C:\Users\jose\Escritorio\cosas facet\Taller de Lenguaje1\2026\tl1-tp10-2026-Joseado381\Tareas\";
---
+++ Tareas/Tareas.cs
@@ -0,0 +1,7 @@
+public class Tareas
+{
+    public int UserID { get; set; }
+    public int Id { get; set; }
+    public string Title { get; set; }
+    public bool Completed { get; set; }
+}
