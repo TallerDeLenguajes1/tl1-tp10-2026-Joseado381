52fbf55331b534e679c04d7a79ee0e1beab3ea09
Joseado381 <josealonzo.jaa@gmail.com>
2026-06-16 12:24:36 -0300

    que le paso al gitignore wtf

--- obj/Debug/net10.0/tl1-tp10-2026-Joseado381.AssemblyInfo.cs
+++ obj/Debug/net10.0/tl1-tp10-2026-Joseado381.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("tl1-tp10-2026-Joseado381")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8ba408a70ecb69257f7fcb82bb342f07be69ccfc")]
 [assembly: System.Reflection.AssemblyProductAttribute("tl1-tp10-2026-Joseado381")]
 [assembly: System.Reflection.AssemblyTitleAttribute("tl1-tp10-2026-Joseado381")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
